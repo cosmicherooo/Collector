7b012da00e85fcbdc2bf8747235c4c484142d246
cosmichero <ivan.karnauhov.99@gmail.com>
2023-10-01 23:51:54 +0300

    Create run_collector.py

---
+++ scripts/run_collector.py
@@ -0,0 +1,11 @@
+import time
+import os
+import pandas as pd
+import math
+from datetime import datetime
+from os.path import join, getsize
+
+from staff.Collector import Collector
+
+def main(): 
+  
