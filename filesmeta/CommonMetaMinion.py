219c7c8bf5a34cbd595bd829637f93740805c158
cosmichero <ivan.karnauhov.99@gmail.com>
2023-10-19 01:28:34 +0300

    Update CommonMetaMinion.py

--- filesmeta/CommonMetaMinion.py
+++ filesmeta/CommonMetaMinion.py
@@ -13,16 +13,15 @@ class CommonMetaMinion():
     def get_meta_inf(self, path):
         try:
 
-            file_name = {"File_name": path.rsplit('/', 1)[-1]}
-            path_to_file = {'File_path': path}
-            size_bytes = {"File_size_bytes": os.stat(path).st_size}
-            size = {"File_size": convert_size(os.stat(path).st_size)}
-            creation_time = {"Creation_time": time.ctime(os.path.getctime(path))}
-            modification_time = {"Modification_time": time.ctime(os.path.getmtime(path))}
+            common_data = {
+                "File_name": path.rsplit('/', 1)[-1],
+                'File_path': path,
+                "File_size_bytes": os.stat(path).st_size,
+                "File_size": convert_size(os.stat(path).st_size),
+                "Creation_time": time.ctime(os.path.getctime(path)),
+                "Modification_time": time.ctime(os.path.getmtime(path))
+            }
 
-            common_data = {**file_name, **path_to_file,
-                           **size_bytes, **size, **creation_time,
-                           **modification_time}
 
             return common_data
 
