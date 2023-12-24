70617027dcbea501da2674fd8a60ff050ec1d9ab
cosmichero <ivan.karnauhov.99@gmail.com>
2023-12-24 23:37:45 +0300

    Update top_10_ext.py

--- utils/top_10_ext.py
+++ utils/top_10_ext.py
@@ -8,6 +8,8 @@ from staff.Collector import Collector
 
 
 
+from collections import Counter
+import itertools
 def top_10_extensions(user, directory_exp, directory_save):
 
 
@@ -27,29 +29,45 @@ def top_10_extensions(user, directory_exp, directory_save):
     if is_db_relevant[0] == 1:
 
         path_to_db = your_collector.directory_save + "/" + str(is_db_relevant[1])
-        your_db = pd.read_csv(path_to_db)
-        your_db['File_extension'] = your_db['File_path'].apply(get_extension)
-        df_extensions_overall = your_db['File_extension'].value_counts()
 
-        df_extensions_overall_top_10 = df_extensions_overall.head(n=10)
+        connection = sqlite3.connect(path_to_db)
+        cursor = connection.cursor()
+        cursor.execute('SELECT File_name FROM files_info')
+        # cursor.execute("ALTER TABLE files_info ADD COLUMN Extension varchar(32)")
+        # names = list(map(lambda x: x[0], cursor.description))
+        files = [file[0] for file in cursor.execute('SELECT File_name FROM files_info')]
+        connection.close()
+
+        extensions_files = list(map(get_extension, files))
+        ext_stat = Counter(extensions_files)
+        ext_stat = dict(sorted(ext_stat.items(), key=lambda x: x[1], reverse=True))
+        ext_stat = dict(itertools.islice(ext_stat.items(), 2))
+
 
         print(f"Your database is relatively new. Here is your top-10 extensions:")
-        print(df_extensions_overall_top_10)
+        print(ext_stat)
 
-        return df_extensions_overall_top_10
+        return ext_stat
 
     elif is_db_relevant[0] == 0:
 
         path_to_db = your_collector.directory_save + "/" + str(is_db_relevant[1])
-        your_db = pd.read_csv(path_to_db)
-        your_db['File_extension'] = your_db['File_path'].apply(get_extension)
-        df_extensions_overall = your_db['File_extension'].value_counts()
+        connection = sqlite3.connect(path_to_db)
+        cursor = connection.cursor()
+        cursor.execute('SELECT File_name FROM files_info')
+        # cursor.execute("ALTER TABLE files_info ADD COLUMN Extension varchar(32)")
+        # names = list(map(lambda x: x[0], cursor.description))
+        files = [file[0] for file in cursor.execute('SELECT File_name FROM files_info')]
+        connection.close()
 
-        df_extensions_overall_top_10 = df_extensions_overall.head(n=10)
+        extensions_files = list(map(get_extension, files))
+        ext_stat = Counter(extensions_files)
+        ext_stat = dict(sorted(ext_stat.items(), key=lambda x: x[1], reverse=True))
+        ext_stat = dict(itertools.islice(ext_stat.items(), 2))
 
         print(f"You need to refresh your hard disk statistics!!! Here is your top-10 extensions:")
-        print(df_extensions_overall_top_10)
-        return df_extensions_overall_top_10
+        print(ext_stat)
+        return ext_stat
 
     elif is_db_relevant is None:
 
